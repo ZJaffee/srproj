7c76366e0dfa97df7ec55c1aee8e61457a3fc5df
Zachary Jaffee <zij@case.edu>
2017-04-15 16:34:15 -0400

    notes on load step

---
+++ load_cscard.py
@@ -0,0 +1,4 @@
+# This file will most likely be responsible for executing a bash script that runs the command
+# COPY college_scorecard(first_name,last_name,dob,email) FROM <file path DELIMITER ',' CSV HEADER;
+# http://stackoverflow.com/questions/2987433/how-to-import-csv-file-data-into-a-postgresql-table
+# http://www.postgresqltutorial.com/postgresql-create-table/
