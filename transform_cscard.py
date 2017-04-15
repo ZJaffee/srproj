b368f075c3ad69e2ba7db1a54ed50f53612151fb
Zachary Jaffee <zij@case.edu>
2017-04-15 16:29:19 -0400

    transform step

--- transform_cscard.py
+++ transform_cscard.py
@@ -1,32 +1,22 @@
 # Notes, PrivacySuppressed needs to be transformed into NULL
-
-
 import csv
-import re
 
-def extract_csv():
-	with open('Most-Recent-Cohorts-All-Data-Elements.csv', 'r+') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',')
-		column_name = list()
-		data_type = list()
-		for val in spamreader.next():
-			column_name.append(val)
-	#zip needs to be delt with differently (as varchar(10))
-	#NULL is numeric, so is PrivacySuppressed 
+csvfile = open("Most-Recent-Cohorts-All-Data-Elements.csv","rb")
+csvout = open("Most-Recent-Cohorts-All-Data-Elements_transformed.csv","wb")
 
-	f = open('schema_cscard.sql', 'w')
-	f.truncate()
-	f.write("CREATE TABLE college_scorecard ( \n")
-	for i in xrange(len(column_name)):
-		feature = "  %s %s,\n"%(column_name[i],data_type[i])
-		f.write(feature)
-	f.write("CONSTRAINT persons_pkey PRIMARY KEY (UNITID) )")
-	f.close()
+reader = csv.reader(csvfile)
+writer = csv.writer(csvout)
 
+def transform_csv():
+	for row in reader:
+	    for i in xrange(len(row)):
+	    	if row[i] == "PrivacySuppressed":
+	    		row[i] = "NULL"
+	    writer.writerow(row) 
 
 
 if __name__ == "__main__":
-    extract_csv()
+    transform_csv()
 
 
 
